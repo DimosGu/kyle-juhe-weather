4d4402844ae3a98491a63fd953d48f23c90f2431
Kyle Tung <dyh920827@gmail.com>
2015-08-24 14:15:34 +0800

    搭建了应用框架，采用 DrawerLayout 呈现城市选择。自定义了天气主界面，并搭配 SwipeRefreshLayout 刷新界面。

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # kyle-juhe-weather
 
 使用聚合数据开发的天气应用
+
+2015-8-24 搭建了应用框架，采用 DrawerLayout 呈现城市选择。自定义了天气主界面，并搭配 SwipeRefreshLayout 刷新界面。
--- app/src/main/java/com/kyletung/kylejuheweather/MainActivity.java
+++ app/src/main/java/com/kyletung/kylejuheweather/MainActivity.java
@@ -1,17 +1,102 @@
 package com.kyletung.kylejuheweather;
 
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+
+    //toolbar
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    SwipeRefreshLayout swipeRefreshLayout;
+
+    //drawer menu
+    ListView listView;
+    String[] drawerCity = {"Beijing", "Hangzhou"};
+    ArrayAdapter<String> drawerAdapter;
+    ImageButton addCity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //init toolbar
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        setSupportActionBar(toolbar);
+        //set actionbar toggle
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_toggle_open, R.string.drawer_toggle_close);
+        actionBarDrawerToggle.syncState();
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        //set status bar color
+        drawerLayout.setStatusBarBackground(R.color.status_bar);
+        //set swipe refresh
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
+        swipeRefreshLayout.setOnRefreshListener(this);
+        swipeRefreshLayout.setColorSchemeResources(R.color.refresh_one, R.color.refresh_two, R.color.refresh_three, R.color.refresh_four);
+        //set drawer menu
+        listView = (ListView) findViewById(R.id.city_list);
+        drawerAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, drawerCity);
+        listView.setAdapter(drawerAdapter);
+        listView.setDividerHeight(0);
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                System.out.println(">>> item long click success <<<");
+                return true;
+            }
+        });
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                System.out.println(">>> item click success <<<");
+            }
+        });
+        addCity = (ImageButton) findViewById(R.id.add_city);
+        addCity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println(">>> add city click success <<<");
+            }
+        });
     }
+
+    @Override
+    public void onRefresh() {
+        Message message = new Message();
+        message.what = 1;
+        handler.sendMessageDelayed(message, 5000);
+    }
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 1:
+                    System.out.println(">>> refresh success <<<");
+                    swipeRefreshLayout.setRefreshing(false);
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
